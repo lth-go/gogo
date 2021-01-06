acd130590facff81fb9539d6dab4b8bbea2bb015
李天华 <lith@weipaitang.com>
2021-01-06 16:52:50 +0800

    fix error

--- vm/error.go
+++ vm/error.go
@@ -20,19 +20,19 @@ const (
 	DYNAMIC_LOAD_WITHOUT_PACKAGE_ERR
 )
 
-var errMessageList []string = []string{
-	"不正确的多字节字符。",
-	"找不到函数$(name)。",
-	"重复定义了函数$(package)#$(name)。",
-	"数组下标越界。数组大小为$(size)，访问的下标为[$(index)]。",
-	"整数值不能被0除。",
-	"引用了null。",
-	"没有找到要加载的文件$(file)",
-	"加载文件时发生错误($(status))。",
-	"重复定义了类$(package)#$(name)。",
-	"没有找到类$(name)。",
-	"对象的类型为$(org)。,不能向下转型为$(target)。",
-	"由于函数$(name)没有指定包，不能动态加载。",
+var errMessageMap map[int]string = map[int]string{
+	BAD_MULTIBYTE_CHARACTER_ERR:      "不正确的多字节字符。",
+	FUNCTION_NOT_FOUND_ERR:           "找不到函数$(name)。",
+	FUNCTION_MULTIPLE_DEFINE_ERR:     "重复定义了函数$(package)#$(name)。",
+	INDEX_OUT_OF_BOUNDS_ERR:          "数组下标越界。数组大小为$(size)，访问的下标为[$(index)]。",
+	DIVISION_BY_ZERO_ERR:             "整数值不能被0除。",
+	NULL_POINTER_ERR:                 "引用了null。",
+	LOAD_FILE_NOT_FOUND_ERR:          "没有找到要加载的文件$(file)",
+	LOAD_FILE_ERR:                    "加载文件时发生错误($(status))。",
+	CLASS_MULTIPLE_DEFINE_ERR:        "重复定义了类$(package)#$(name)。",
+	CLASS_NOT_FOUND_ERR:              "没有找到类$(name)。",
+	CLASS_CAST_ERR:                   "对象的类型为$(org)。,不能向下转型为$(target)。",
+	DYNAMIC_LOAD_WITHOUT_PACKAGE_ERR: "由于函数$(name)没有指定包，不能动态加载。",
 }
 
 func vmError(errorNumber int, a ...interface{}) {
@@ -44,7 +44,7 @@ func vmError(errorNumber int, a ...interface{}) {
 
 	fmt.Println("运行错误")
 	//fmt.Printf("Line: %d\n", getLineNumberByPc(exe, functionList, pc))
-	log.Fatalf("%d\n%s\n", errorNumber, errMessageList[errorNumber])
+	log.Fatalf("%d\n%s\n", errorNumber, errMessageMap[errorNumber])
 	//fmt.Printf(errMessageMap[errorNumber], a...)
 }
 
